56139970f032ae16f1cbc2360301043354f3fadd
devmfundo <devmfundo@gmail.com>
2024-08-29 13:17:25 +0200

    added a class TodoItem,function to add items to an array

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import "./styles.css";
 
-class AddTodoItem {
+class TodoItem {
     constructor(title, description, notes, dueDate, priority) {
             this.title = title,
             this.description = description,
-            this.notes,
+            this.notes = notes,
             this.dueDate = dueDate,
             this.priority = priority
     }
 }
-const item1 = new AddTodoItem("Shopping","Groceries","Something to sustain myself with","today","High");
-console.log(item1.title);
+const title = prompt("Title?");
+const description = prompt("Description?");
+const notes = prompt("notes?");
+const dueDate = prompt("due?");
+const priority = prompt("priority?");
+const item = new TodoItem(title, description, notes, dueDate, priority);
+
 const toDoItems=[];
+
+const addItem=(()=>{
+    toDoItems.unshift({item})
+})();
+console.log(toDoItems);
 const projectsToDo=[];
 
-//function to add items to toDoitems[].
+
 //function to add selected projects to projectsToDo[].
-//Add local storage.
+//Add local storage. 
 //Project function to make a project then add toDo items later.
 //due date should count using a calender date and time and minus the difference and be able to say due in "what day and time".
